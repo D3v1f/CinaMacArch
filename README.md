08fe6408b8208de8dc2e36f45af65641b5f1cc48
D3v1f <caizadavid417@gmail.com>
2024-07-29 08:51:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# CinaMac 🖥️
+Cinamon Mac Style
+## Setup 
+
+<img src='.assets/desktopGeneral.png'>
+
+## Ten en cuenta ⚙️
+* **OS** : Linux Mint
+* **Terminal** : Alacritty
+* **Wallpaper** : 
+* **File Manger** : Thunar
---
+++ cinaMacInstall.sh
@@ -0,0 +1,6 @@
+clear
+echo "              ••••••••••••••••••••••••••••••••••••••••••••••"
+echo "              •             CinaMac Dotfile                •"
+echo "              •                   D3v1f                    •"
+echo -e  "              •           \e[32mhttps://github.com/D3v1f\e[0m         •" 
+echo "              ••••••••••••••••••••••••••••••••••••••••••••••"
